48f6e3dae52ce85c83600465dcd35c812588ff2c
RomanTabi <roman.tabi@gmail.com>
2016-11-13 17:15:08 +0100

    Add Makefile and base .cpp file

---
+++ src/disparity_map_estimation.cpp
@@ -0,0 +1,14 @@
+#include <caffe/caffe.hpp>
+using caffe::Blob;
+using caffe::Caffe;
+using caffe::Net;
+using caffe::shared_ptr;
+using caffe::vector;
+
+int main() {
+    Net<float> *caffe_net;
+
+    std::cout << "Disparity map estimation" << std::endl;
+
+    return 0;
+}
